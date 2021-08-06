ee33b0efbdac3c0a2e7cc52fa7fed4305e6e9bd1
suriel_asael <kato_1350@hotmail.com>
2021-08-05 19:24:48 -0500

    add: add children routes and lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(  m => m.AuthModule )
+  },
   {
     path: '404',
     component: ErrorPageComponent
