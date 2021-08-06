f824c037416c2ecf016846f4500a4027063b6cfc
suriel_asael <kato_1350@hotmail.com>
2021-08-05 23:45:59 -0500

    add: add heroe interface

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
@@ -9,11 +10,15 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class ListadoComponent implements OnInit {
 
+  heroes: Heroe[] = [];
+
   constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
     this.heroesService.getHeroes()
-      .subscribe( resp => console.log( resp ) );
+      .subscribe( heroes => {
+        this.heroes = heroes;
+      });
   }
 
 }
