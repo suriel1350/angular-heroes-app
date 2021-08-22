56726914efeaf02f3036d9977a2e74b269ce28f7
suriel_asael <kato_1350@hotmail.com>
2021-08-21 19:21:34 -0500

    add: add process for deleting heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -71,4 +71,10 @@ export class AgregarComponent implements OnInit {
 
   }
 
+  borrarHeroe(){
+    this.heroesService.borrarHeroe( this.heroe.id! )
+      .subscribe( resp => {
+        this.router.navigate(['/heroes'])
+      });
+  }
 }
