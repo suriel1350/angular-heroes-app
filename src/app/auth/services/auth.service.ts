4db014f89b74d08fdd8cca249da67cbac1d9254c
suriel_asael <kato_1350@hotmail.com>
2021-08-22 22:22:30 -0500

    add: add localStorage

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 
 import { Auth } from '../interfaces/auth.interface';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,11 +20,25 @@ export class AuthService {
 
   constructor( private http: HttpClient ) { }
 
+  verificaAutenticacion():  Observable<boolean>{
+    if( !localStorage.getItem('token') ){
+      return of(false);
+    }
+
+    return this.http.get<Auth>(`${ this.baseUrl }/usuarios/1`)
+      .pipe(
+        map( auth => {
+          this._auth = auth;
+          return true;
+        })
+      );
+  }
+
   login() {
     return this.http.get<Auth>(`${ this.baseUrl }/usuarios/1`)
               .pipe(
                 tap( auth => this._auth = auth ),
-                tap( auth => localStorage.setItem('id', auth.id) )
+                tap( auth => localStorage.setItem('token', auth.id) )
               );
   }
 
